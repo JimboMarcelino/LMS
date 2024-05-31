fc73cbaab0a1b3f8b065d4eec8426e924cc3bfb4
JimboMarcelino <92971749+JimboMarcelino@users.noreply.github.com>
2024-05-31 19:49:05 +0800

    Add files via upload

---
+++ Library Management System/Library_Management_System.java
@@ -0,0 +1,17 @@
+
+package library_management_system;
+
+
+public class Library_Management_System {
+
+    
+    public static void main(String[] args) {
+        
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+        
+    }
+    
+}
